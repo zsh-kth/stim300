e45c95280cf959534e2d2c5af6df974d2d4a8e61
zsh-kth <102040413+zsh-kth@users.noreply.github.com>
2022-04-05 10:47:36 +0200

    correct gravity and change acc scale

--- src/stim300_constants.h
+++ src/stim300_constants.h
@@ -16,6 +16,8 @@ static constexpr uint8_t N_BYTES_CRC = 4;
 static constexpr uint8_t N_BYTES_STATUS = 1;
 static constexpr uint8_t N_BYTES_TERMINATION = 2;
 static constexpr uint8_t MAX_DATAGRAM_SIZE = 65;
+  
+static constexpr double STIM300_GRAVITY = 9.80665;
 
 enum class DatagramIdentifier {
   RATE,
@@ -243,15 +245,15 @@ constexpr uint32_t powerOf2(uint8_t power) { return 1 << power; }
 static constexpr double accScale(AccRange acc_range) {
   switch (acc_range) {
   case AccRange::G2:
-    return 1.0 / powerOf2(21);
+    return STIM300_GRAVITY / powerOf2(21);
   case AccRange::G5:
-    return 1.0 / powerOf2(20);
+    return STIM300_GRAVITY / powerOf2(20);
   case AccRange::G10:
-    return 1.0 / powerOf2(19);
+    return STIM300_GRAVITY / powerOf2(19);
   case AccRange::G30:
-    return 1.0 / powerOf2(18);
+    return STIM300_GRAVITY / powerOf2(18);
   case AccRange::G80:
-    return 1.0 / powerOf2(16);
+    return STIM300_GRAVITY / powerOf2(16);
   }
 }
 
@@ -274,7 +276,7 @@ static constexpr double gyroIncrScale() {
   return (M_PI / 180.00) / powerOf2(21);
 }
 static constexpr double gyroScale() { return (M_PI / 180.00) / powerOf2(14); }
-static constexpr double inclScale() { return 1.0 / powerOf2(22); }
+static constexpr double inclScale() { return STIM300_GRAVITY / powerOf2(22); }
 static constexpr double inclIncrScale() { return 1.0 / powerOf2(25); }
 static constexpr double tempScale() { return 1.0 / powerOf2(8); }
 static constexpr double auxScale() { return 5.0 / powerOf2(24); }
